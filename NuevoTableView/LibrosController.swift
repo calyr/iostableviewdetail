ebd66bf152a357f9db4b3db4f87fac8ebcd8550c
calyr <calyr.software@gmail.com>
2016-07-22 09:57:11 -0400

    Init

---
+++ NuevoTableView/LibrosController.swift
@@ -0,0 +1,54 @@
+//
+//  LibrosController.swift
+//  NuevoTableView
+//
+//  Created by Roberto Carlos Callisaya Mamani on 7/22/16.
+//  Copyright © 2016 Roberto Carlos Callisaya Mamani. All rights reserved.
+//
+
+import UIKit
+
+var toDoItems : [String] = []
+
+class LibrosController: UITableViewController {
+
+    @IBOutlet var tableBooks: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    internal override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return toDoItems.count
+        
+    }
+    internal override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoItems[indexPath.row]
+        
+        return cell
+    }
+    
+    internal override func viewWillAppear(animated: Bool) {
+        tableBooks?.reloadData()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NuevoTableView/ViewController.swift
+++ NuevoTableView/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtIsbn: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buscar(sender: UITextField) {
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
